ff9165a4a2472850cfccb4bb82b67fd7961a097e
Graham Anderson <grahama@me.com>
2017-05-30 13:14:07 -0500

    Todo Search tests

--- app/tests/components/TodoSearch.test.jsx
+++ app/tests/components/TodoSearch.test.jsx
@@ -1,1 +1,33 @@
-rclass  
+import React from 'react'
+import ReactDom from 'react-dom'
+import expect from 'expect'
+import $ from 'jQuery'
+import ReactTestUtils from 'react-dom/test-utils'
+
+import TodoSearch from 'TodoSearch'
+
+describe('TodoSearch', () => {
+  it('should exist', () => {
+    expect(TodoSearch).toExist()
+  })
+
+  it('should call onSearch with entered input text', () => {
+    const searchText = 'Dog'
+    const spy = expect.createSpy()
+    const todoSearch = ReactTestUtils.renderIntoDocument(<TodoSearch onSearch={spy} />)
+
+      todoSearch.searchText.value = searchText
+      ReactTestUtils.Simulate.change(todoSearch.searchText)
+      expect(spy).toHaveBeenCalledWith(false, 'Dog')
+    })
+
+    it('should call onSearch with checkbox change', () => {
+      const spy = expect.createSpy()
+      const todoSearch = ReactTestUtils.renderIntoDocument(<TodoSearch onSearch={spy} />)
+
+        todoSearch.showCompleted.checked = true
+        ReactTestUtils.Simulate.change(todoSearch.showCompleted)
+
+        expect(spy).toHaveBeenCalledWith(true, '')
+      })
+})
