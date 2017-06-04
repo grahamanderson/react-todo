c3cf3ce7e613c4cfd4623023596056ec3ace246a
Graham Anderson <grahama@me.com>
2017-06-04 16:39:13 -0500

    move firebase creds to environment vars

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,10 @@ import firebase from 'firebase'
 
 try {
   var config = {
-      apiKey: "AIzaSyDgiTyJtpnVFGQXz6ezChdamsjmAKaxt3E",
-      authDomain: "graham-todo-app.firebaseapp.com",
-      databaseURL: "https://graham-todo-app.firebaseio.com",
-      projectId: "graham-todo-app",
-      storageBucket: "graham-todo-app.appspot.com",
-      messagingSenderId: "512316965435"
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL:process.env.DATABASE_URL ,
+      storageBucket: process.env.STORAGE_BUCKET,
     };
 
     firebase.initializeApp(config);
