14cd8143ad6de0f68d4c5a33fe9c515be57572e5
Graham Anderson <grahama@me.com>
2017-06-05 12:11:04 -0500

    Update index.js

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -1,17 +1,35 @@
-import firebase from 'firebase';
+// Hardcoding the environment variables (below) works
+// import firebase from 'firebase';
 
-const API_KEY = '****************************'
-const AUTH_DOMAIN = '*********************'
-const DATABASE_URL= '*******************'
-const STORAGE_BUCKET=''
+// const API_KEY = '****************************'
+// const AUTH_DOMAIN = '*********************'
+// const DATABASE_URL= '*******************'
+// const STORAGE_BUCKET=''
+
+
+// try {
+//   var config = {
+//     apiKey: API_KEY,
+//     authDomain: AUTH_DOMAIN,
+//     databaseURL: DATABASE_URL,
+//     storageBucket: STORAGE_BUCKET,
+//   };
+
+//   firebase.initializeApp(config);
+// } catch (e) {
+
+// }
+
+
+// console.log('api key: ', config.apiKey)
 
 
 try {
   var config = {
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    databaseURL: DATABASE_URL,
-    storageBucket: STORAGE_BUCKET,
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
   };
 
   firebase.initializeApp(config);
@@ -19,24 +37,7 @@ try {
 
 }
 
-
-console.log('api key: ', config.apiKey)
-
-
-// try {
-//   var config = {
-//     apiKey: process.env.API_KEY,
-//     authDomain: process.env.AUTH_DOMAIN,
-//     databaseURL: process.env.DATABASE_URL,
-//     storageBucket: process.env.STORAGE_BUCKET,
-//   };
-//
-//   firebase.initializeApp(config);
-// } catch (e) {
-//
-// }
-//
-// console.log('api key: ', process.env.API_KEY)
+console.log('api key: ', process.env.API_KEY)
 
 
 export var githubProvider = new firebase.auth.GithubAuthProvider();
